43df21ee9d13e3eaf8eae05318dfa9a4c097cc19
jpafcampos <jpafcampos@gmail.com>
2019-05-10 18:44:39 -0300

    sex18:44

--- Neighborhood4.m
+++ Neighborhood4.m
@@ -1,38 +1,40 @@
-function [Vfim, Cl] = Neighborhood4(Vinit, C)
-     Vfim = Vinit;
+function [Vfim, Cl, D] = Neighborhood4(Vinit,Vpa,Xc,Yc,Xpa,Ypa,C,D, prob)
+    %Liga 2 pontos aleatorios e desliga 2 pontos aleatorios 
+    Vfim = Vinit;
     n = length(Vinit);
     count0 = 0;
     count1 = 0;
     p = 1;
     r = randperm(n);
     i = 1;
-    zero = [0,0];
-    um = [0,0];
-    
+    j = 1;
+    pontos_desligados = [0,0];
+    pontos_ligados = [0,0];
     while ((count1 < 2) || (count0 < 2))
         if Vfim(r(p)) == 1
             if count1 < 2
                 Vfim(r(p)) = not(Vinit(r(p)));
+                pontos_desligados(j) = r(p);
                 count1 = count1 + 1;
                 p = p + 1;
-                
+                j = j + 1;
             end
         end
         if Vfim(r(p)) == 0
             if count0 < 2 
                 Vfim(r(p)) = not(Vinit(r(p)));
                 count0 = count0 + 1;
-                zero(i) = r(p);
+                pontos_desligados(i) = r(p);
                 p = p + 1;
                 i = i + 1;
             end
         end
         p = p + 1;
     end
-
-    C(zero(1),:) = zeros(1,500);
-    C(zero(2),:) = zeros(1,500);
-    Cl = C;
-
+    
+    [Cl, D] = desliga_pa( pontos_desligados(1),Vpa,Xc,Yc,Xpa,Ypa,C, D );
+    [Cl, D] = desliga_pa( pontos_desligados(2),Vpa,Xc,Yc,Xpa,Ypa,Cl,D);
+    [Cl, D] = liga_pa( pontos_ligados(1),Vpa,Xc,Yc,Xpa,Ypa,Cl,D, prob);
+    [Cl, D] = liga_pa( pontos_ligados(2),Vpa,Xc,Yc,Xpa,Ypa,Cl,D, prob);
 end
 
