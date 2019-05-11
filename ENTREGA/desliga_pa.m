294f2cd121793ca353b1aeee92d80a4bb5eb3aff
italoaugustoandrade <italoaugustoandrade@gmail.com>
2019-05-11 16:04:24 -0300

    Pasta entrega - numeropa

---
+++ ENTREGA/desliga_pa.m
@@ -0,0 +1,69 @@
+function [ nC,nD, nVpa,ngVpa ] = desliga_pa( idVpa,Vpa,Xc,Yc,Xpa,Ypa,C,D,gVpa)
+
+
+%Inicialização das variáveis
+nC=C;
+nD=D;
+nVpa=Vpa;
+ngVpa=gVpa;
+nPA=length(Xpa);
+Nc=length(Xc);
+pC=ones(nPA,Nc);
+
+
+for i=1:nPA
+    for j=1:Nc
+        D(i,j)=sqrt((Xpa(i)-Xc(j))^2+(Ypa(i)-Yc(j))^2);
+        if (D(i,j)>85)
+            pC(i,j)=0;
+        end
+    end
+end
+
+
+id=find(C(idVpa,:)==1);
+tid=length(id);
+pC(idVpa,:)=0;
+nC(idVpa,:)=pC(idVpa,:);
+
+
+if (tid>0)
+    for i=1:tid
+        j=id(i);
+        idc=find(pC(:,j)==1);
+        tidc=length(idc);
+        gVpa(j)=0;
+        if (tidc>0)
+            g=randi(tidc,1,1);
+            
+            pC(:,j)=0;
+            nC(:,j)=pC(:,j);
+            pC(idc(g),j)=1;
+            nC(idc(g),j)=pC(idc(g),j);
+            gVpa(j)=id(g);
+        end
+    end
+end
+
+nVpa(idVpa)=0;
+
+gXpa=Xpa;
+gYpa=Ypa;
+for i=1:nPA
+gXpa(i)=Xpa(i)*nVpa(i);
+gYpa(i)=Ypa(i)*nVpa(i);
+end
+
+figure(2)
+psize=30;
+ngVpa=gVpa;
+scatter(Xc(:), Yc(:), psize, gVpa(:));
+colormap(prism(nPA))
+hold on
+scatter(gXpa(:), gYpa(:), psize,(1:64),'*');
+
+
+
+
+end
+
--- ENTREGA/main.m
+++ ENTREGA/main.m
@@ -9,3 +9,6 @@ Yc=table2array(data(:,2));
 B=table2array(data(:,3));
 
 [ Xpa, Ypa, Vpa, C,D, gVpa ] = solucao_inicial(Xc,Yc,B);
+fpa=numero_pa(Vpa, Xpa, Ypa, Xc, Yc,C,B);
+
+%[ nC,nD, nVpa,ngVpa ] = desliga_pa( idVpa,Vpa,Xc,Yc,Xpa,Ypa,C,D,gVpa);
