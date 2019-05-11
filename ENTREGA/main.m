55d8283445ffc771d0f6319b9909d796cad97f1e
italoaugustoandrade <italoaugustoandrade@gmail.com>
2019-05-11 16:18:26 -0300

    Pasta entrega - numeropa

--- ENTREGA/main.m
+++ ENTREGA/main.m
@@ -9,6 +9,16 @@ Yc=table2array(data(:,2));
 B=table2array(data(:,3));
 
 [ Xpa, Ypa, Vpa, C,D, gVpa ] = solucao_inicial(Xc,Yc,B);
-fpa=numero_pa(Vpa, Xpa, Ypa, Xc, Yc,C,B);
 
+
+fpa=numero_pa(Vpa, Xpa, Ypa, Xc, Yc,C,B);
+idVpa=10;
+m=1;
+while(m<10)
+    
+[ C,D, Vpa,gVpa ]=desliga_pa( idVpa,Vpa,Xc,Yc,Xpa,Ypa,C,D,gVpa);
+
+fpa=numero_pa(Vpa, Xpa, Ypa, Xc, Yc,C,B);
+m=m+1;
+end
 %[ nC,nD, nVpa,ngVpa ] = desliga_pa( idVpa,Vpa,Xc,Yc,Xpa,Ypa,C,D,gVpa);
