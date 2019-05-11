db1f4416354a874d9edb73347547ac68b908107c
italoaugustoandrade <italoaugustoandrade@gmail.com>
2019-05-11 20:32:48 -0300

    Pasta entrega - numeropa

---
+++ ENTREGA/main2.m
@@ -0,0 +1,57 @@
+clc
+clear all
+close all
+
+%Leitura dos dados de entrada
+data = readtable('clientes.csv');
+Xc=table2array(data(:,1));
+Yc=table2array(data(:,2));
+B=table2array(data(:,3));
+
+[ Xpa, Ypa, Vpa, C,D ] = solucao_inicial(Xc,Yc,B);
+
+idVpa=10;
+
+for j=1:500
+    for k=1:64
+        if (C(k,j)==1)
+            v(j)=k;
+        end
+    end
+end
+
+fpa=numero_pa(Vpa, Xpa, Ypa, Xc, Yc,C,B);
+[ C,D, Vpa ]=desliga_pa( idVpa,Vpa,Xc,Yc,Xpa,Ypa,C,D);
+for i=1:6
+    Xpa(1)=Xpa(1)+10;
+    [ C,D, Vpa ]=desliga_pa( idVpa,Vpa,Xc,Yc,Xpa,Ypa,C,D);
+      for j=1:500
+        for k=1:64
+            if (C(k,j)==1)
+                v(1,j)=k;
+            end
+        end
+    end
+    fpa=numero_pa(Vpa, Xpa, Ypa, Xc, Yc,C,B);
+    [ C,D, Vpa ]=liga_pa( idVpa,Vpa,Xc,Yc,Xpa,Ypa,C,D);
+      for j=1:500
+        for k=1:64
+            if (C(k,j)==1)
+                v(1,j)=k;
+            end
+        end
+    end
+    fpa=numero_pa(Vpa, Xpa, Ypa, Xc, Yc,C,B);
+    
+    for j=1:500
+        for k=1:64
+            if (C(k,j)==1)
+                v(1,j)=k;
+            end
+        end
+    end
+ %   idVpa=randi(63)+1;
+%    v(2,i)=idVpa;
+end
+
+
