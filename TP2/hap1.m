d3620f509fcd38d0c251709f78ef383bb42c2bd0
italoaugustoandrade <italoaugustoandrade@gmail.com>
2019-06-09 16:49:08 -0300

    Main + HAP

--- TP2/hap1.m
+++ TP2/hap1.m
@@ -1,8 +1,17 @@
 function [ Ranking ] = hap1( MatrizCandidatos,Prioridade )
 
-MatrizAval=MatrizCandidatos*Prioridade;
-Resultado=
+MatrizAval=MatrizCandidatos;
+for i=1:length(Prioridade)
+    MatrizAval(:,i)=MatrizCandidatos(:,i)*Prioridade(i);
+end
 
+T=size(MatrizCandidatos);
+Result=zeros(T(1),1);
+for i=1:T(1)
+    Result(i)=mean(MatrizAval(i,:));
+end
+
+[~, Ranking]=sort(Result);
 
 end
 
