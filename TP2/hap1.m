9c395b7d062c5ccb9317646f78dde531e36f5789
italoaugustoandrade <italoaugustoandrade@gmail.com>
2019-06-09 17:51:42 -0300

    Main + HAP

--- TP2/hap1.m
+++ TP2/hap1.m
@@ -1,17 +1,36 @@
-function [ Ranking ] = hap1( MatrizCandidatos,Prioridade )
-
-MatrizAval=MatrizCandidatos;
-for i=1:length(Prioridade)
-    MatrizAval(:,i)=MatrizCandidatos(:,i)*Prioridade(i);
-end
+function [ Ranking ] = hap1( MatrizCandidatos,PrioridadeGeral )
 
 T=size(MatrizCandidatos);
-Result=zeros(T(1),1);
-for i=1:T(1)
-    Result(i)=mean(MatrizAval(i,:));
+PrioridadeCriterio=zeros(T(1),T(2));
+VetorResultado=zeros(T(1),1);
+for k=1:T(2)
+    C1=MatrizCandidatos(:,k);
+    MatrizC1=zeros(T(1),T(1));
+    
+    for i=1:T(1)
+        for j=1:T(1)
+            MatrizC1(i,j)=C1(i)/C1(j);
+        end
+    end
+    MatrizNormalizada=MatrizC1;
+    for i=1:T(1)
+        MatrizNormalizada(:,i)=MatrizNormalizada(:,i)/sum(MatrizC1(:,i));
+    end
+    
+    for i=1:T(1)
+        PrioridadeCriterio(i,k)=mean(MatrizNormalizada(i,:));
+    end
+    
+end
+MatrizPrioridade=PrioridadeCriterio;
+for i=1:T(2)
+    MatrizPrioridade(:,i)=PrioridadeCriterio(:,i)*PrioridadeGeral(i);
 end
 
-[~, Ranking]=sort(Result);
+for i=1:T(1)
+    VetorResultado(i)=sum(MatrizPrioridade(i,:));
+end
+[~, Ranking]=sort(VetorResultado);
 
 end
 
--- TP2/main.m
+++ TP2/main.m
@@ -1,5 +1,17 @@
 %Construção das matriz de pesos
-sCriterios=['Número de PA', 'Distância', 'Clientes atendidos', 'Porcentagem média de banda utilizada'];
+clear all
+sol = ...
+  [28 25212.718533046776 0.976 78.923992;
+   29 24391.680869004915 0.992 60.028025000000007;
+   34 23702.176059187877 0.988 47.10124;
+   39 22271.383728665267 0.982 56.801011;
+   41 21066.912788484646 0.986 52.982693;
+   46 19796.92213818492 0.994 51.693423;
+   44 18722.024406456432 0.974 62.584965000000004;
+   50 17572.470519005314 0.984 66.921523;
+   56 16207.389940555264 0.996 45.087979000000004];
+
+sCriterios=['Número de PA', 'Distância', 'Clientes atendidos', 'Banda máxima'];
 nCriterios=4;
 MatrizPesos=[1,5,7,9];
 MatrizPesos(2,:)=[1/MatrizPesos(1,2),1,3,7];
@@ -15,6 +27,8 @@ for i=1:nCriterios
 Prioridade(i)=mean(MatrizNormalizada(i,:));
 end
 
-MatrizCandidatos=[10,20,10,15;15,25,10,30];
+MatrizCandidatos=sol;
+MatrizCandidatos(:,3)=1-sol(:,3);
+
 
 Ranking = hap1( MatrizCandidatos,Prioridade );
