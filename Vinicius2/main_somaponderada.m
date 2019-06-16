892fa2a328b91e8ea179054837d7a1428d4011c0
jpafcampos <jpafcampos@gmail.com>
2019-06-16 15:42:00 -0300

    pareto

--- Vinicius2/main_somaponderada.m
+++ Vinicius2/main_somaponderada.m
@@ -11,13 +11,13 @@ B=table2array(data(:,3));
 %quanto maior o peso 'w', maior a enfase dada a função 2 - dist
 %SA_soma_ponderada(alpha, Epsilon, Xc, Yc, B, w)
 
-w = [0.1:0.1:0.9];
-for i = 1:9
+w = [0.1:0.1/2:0.9];
+for i = 1:length(w)
     pontos_carregados = 0;
     [vbest, xbest, ybest, Cbest, f, f1, f2] = SA_soma_ponderada(0.9, 1e-1, Xc, Yc, B, w(i));
     num_Pa = floor(f1 * 75 + 25);
     dist = f2 * 15000 + 15000;
-    cobertura = sum(sum(Cbest))/500;
+    cobertura = sum(sum(Cbest));
     for j=1:length(xbest)
         b(j) = (B'*Cbest(j,:)');
         if b(j) > 25
